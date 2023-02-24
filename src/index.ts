7add6f14f99e8b7ff8f7cdd1143bffae69a9258a
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-02-24 09:35:17 -0300

    feat(index.ts): adição de middleware para header em response
    permissão de qualquer origem com 'Access-Control-Allow-Origin'

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import 'express-async-errors';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 
+import { middlewareCors } from './helpers/MiddlewareCors.js';
 import { middlewareError } from './helpers/MiddlewareError.js';
 import { loginRouter } from './modules/login/loginRouter.js';
 import { userRouter } from './modules/user/userRouter.js';
 import { usersRouter } from './modules/users/usersRouter.js';
+
 dotenv.config();
 
 const server = express();
 server.use(express.json());
+server.use(cors());
 
 server.use('/user', userRouter);
 server.use('/users', usersRouter);
@@ -17,5 +21,6 @@ server.use('/users', usersRouter);
 server.use('/login', loginRouter);
 
 server.use(middlewareError);
+server.use(middlewareCors);
 
 server.listen(3333, () => console.log('Rodando na porta => 3333'));
