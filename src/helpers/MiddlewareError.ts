f98981b5a8e61d33bc8e428e86c261c171f55ad1
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-02-25 14:57:20 -0300

    test: enviando título do erro em response

--- src/helpers/MiddlewareError.ts
+++ src/helpers/MiddlewareError.ts
@@ -2,16 +2,20 @@ import { NextFunction, Request, Response } from 'express';
 
 import { ApiError } from './ApiErrors.js';
 
+interface ResponseCustom {
+  statusText: string;
+}
+
 export const middlewareError = (
   error: Error & Partial<ApiError>,
   req: Request,
-  res: Response,
+  res: Response & ResponseCustom,
   next: NextFunction,
 ) => {
   const status = error.status ?? 500;
   const message = error.message ?? 'Internal Server Error';
   const statusMessage = error.errorName ?? 'Error';
-  res.statusMessage = statusMessage;
+  res.statusText = statusMessage;
 
   res.status(status).json({ message, statusMessage });
   next();
